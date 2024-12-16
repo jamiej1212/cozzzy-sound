5fb2f53d099d845cb3f252aa89ff8c39761c2b0a
Jamie J <jaydee12347@gmail.com>
2024-12-16 06:02:25 -0800

    :wrench: update next-videos configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+import { withNextVideo } from "next-video/process";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withVideos = require('next-videos')
+
+module.exports = withVideos()
+
+export default withNextVideo(nextConfig);
